731fc0543147f7c107096127dcd082ce11102789
siwonlee <[swleeca@gmail.com](mailto:swleeca@gmail.com)>
2026-02-02 19:59:46 -0500

    refresh error

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,7 +8,7 @@ use App\Models\Apl;
 
 class SearchController extends Controller
 {
-    
+
     public function __construct()
     {
       //  $this->middleware('auth');
@@ -18,26 +18,26 @@ class SearchController extends Controller
     public function index()
     {
 
-       
+
        // return view('search')->with('upcs','') ;
         return view('search')->with(['upcs'=>'', 'cd_output'=>'start']) ;
 
     }
-   
-        public function refresh($state)
+
+        public function refresh($state = 'start')
     {
 
-       
+
        // return view('search')->with('upcs','') ;
         return view('search')->with(['upcs'=>'', 'cd_output'=>'start', 'sstate'=> $state]) ;
 
     }
-    
+
 
     public function general(Request $request)
     {
 
-       
+
 $upc = $request->input('upc');
 $sstate = $request->input('sstate');
 
@@ -70,7 +70,7 @@ $sstate = $request->input('sstate');
               )->where('upc', 'like', '%'.$upc.'%')->get() ;
     }
 
-    
+
 
 // dd($result);
 
@@ -80,7 +80,7 @@ if($upc != '' and  !(strlen($upc)==12 or strlen($upc)==13 or strlen($upc)==8 or 
       {
 
         $cd_output = "The upc length should be 8, 12 or 13 including a check digit at the end. ";
-  
+
 
       }
       if($upc != '' and  (strlen($upc)==12 or strlen($upc)==13 or strlen($upc)==8 ))
@@ -94,12 +94,12 @@ if($upc != '' and  !(strlen($upc)==12 or strlen($upc)==13 or strlen($upc)==8 or 
             $f2 = $e2%10;
             $g2 = 10-$f2;
              if($g2==10){$g2=0;	}
-            
+
              if($b2[11]==$g2){$cv = '1'; }else{$cv = '0';}
-             
+
              $g=$g2;
              }
-             
+
             if(strlen($upc)==13){
             $upc_left3=substr($upc,0,13);
             $b3 = str_split($upc_left3);
@@ -109,9 +109,9 @@ if($upc != '' and  !(strlen($upc)==12 or strlen($upc)==13 or strlen($upc)==8 or 
             $f3 = $e3%10;
             $g3 = 10-$f3;
              if($g3==10){$g3=0;	 }
-             
+
              if($b3[12]==$g3){$cv = '1'; }else{$cv = '0';}
-               
+
              $g=$g3;
              }
 
@@ -119,17 +119,17 @@ if($upc != '' and  !(strlen($upc)==12 or strlen($upc)==13 or strlen($upc)==8 or 
 
             if(strlen($upc)==8){
  $cv = '';
-              
-    
+
+
              }
 
-             
 
-     
+
+
 
       //dd($data_indi);
 
-  
+
   if($cv == '0'){ $cd_output = " It has a wrong check digit.  It should be ".$g; }
 
    }
@@ -154,8 +154,8 @@ if($upc != '' and  !(strlen($upc)==12 or strlen($upc)==13 or strlen($upc)==8 or 
 
 return view('search')->with(['upcs'=>$result,'upc'=>$upc, 'cd_output'=>$cd_output, 'sstate'=>$sstate]);
 
-       
-        
+
+
     }
 
 
@@ -164,7 +164,7 @@ return view('search')->with(['upcs'=>$result,'upc'=>$upc, 'cd_output'=>$cd_outpu
     public function apl()
     {
 
-       
+
 //$upc = $request->input('upc');
 
 //$result = Upc::search($request->get('upc'))->paginate(10);
@@ -205,8 +205,8 @@ return $upcs['upcs'];
 //return "siwon";
 // return view('temp')->with($upcs);
 // return view('search')->with(['upcs'=>$result,'upc'=>$upc]);
-      
-        
+
+
     }
 
     public function aplSearch($upc)
@@ -273,18 +273,18 @@ return $upcs['upcs'];
         "cate_desc",
         "subcategory",
         "subcate_desc",
-       
+
         "description",
-  
+
         "uom",
-    
+
         "state",
         "exchange",
-       
+
         ) -> where('state', $sstate)->where('upc', $upc)->get() ;
       }
         //$this->cate = $cate;
-       
+
 
        //dd($upcs);
       //  return view('temp')->with($upcs);
@@ -299,101 +299,101 @@ return $upcs['upcs'];
         return view('searchsimple')->with(['upcs'=>$result,'upc'=>$upc]);
     }
 
- 
 
 
 
-    
+
+
     public function edit(Request $request, $id)
     {
- 
- 
+
+
         $request->validate([
-        
+
             'upc' => 'required',
             'category_full' => 'required',
             'subcat_full' => 'required',
             'brand' => 'required',
             'description' => 'required',
             'short_desc' => 'required',
-    
+
             'high_cost' => 'required',
             'benefit_qt' => 'required',
             'benefit_uom_wow' => 'required',
             'verify_date' => 'required',
-         
+
 
         ]);
 
- 
-  
+
+
 
 
         $time = $request->input('time');
         $staff = $request->input('staff');
-    
-        $upc = $request->input('upc');          
-        
-      
-  
-  
+
+        $upc = $request->input('upc');
+
+
+
+
         $verify_date = $request->input('verify_date');
         $category_full = $request->input('category_full');
         $subcat_full = $request->input('subcat_full');
         $brand = $request->input('brand');
         $description = $request->input('description');
         $short_desc = $request->input('short_desc');
- 
+
         $high_cost = $request->input('high_cost');
-  
+
         $benefit_qt = $request->input('benefit_qt');
-        
+
         $benefit_uom_wow = $request->input('benefit_uom_wow');
-   
-    
- 
+
+
+
          $end_date=$request->input('end_date');
-  
+
         $plu_indicator=$request->input('plu_indicator');
         $broadband_flag=$request->input('broadband_flag');
-    
+
         $benefit_uom_wow = strtoupper($benefit_uom_wow);
 
 
 
         Upc::where('id', $id)
               ->update([
- 
-       
+
+
                  'edit_date'=>$time,
                  'edit_staff'=>$staff,
-        
-    
+
+
                'category_full'=>   $category_full,
-            'subcat_full'=>     $subcat_full, 
-               'brand'=>  $brand, 
+            'subcat_full'=>     $subcat_full,
+               'brand'=>  $brand,
               'description'=>    $description,
                'short_desc'=>   $short_desc,
                'long_desc'=>   $short_desc,
-      
-  
+
+
                'high_cost'=>   $high_cost,
             'benefit_uom_wow'=>    $benefit_uom_wow,
             'benefit_qt' => $benefit_qt,
            'end_date'=>       $end_date,
-           'verify_date'=>       $verify_date, 
+           'verify_date'=>       $verify_date,
            'plu_indicator'=>$plu_indicator,
            'broadband_flag'=>$broadband_flag,
-                 
+
                  ]);
- 
+
 
 
 
 
               return redirect()->back()->with('approved','The item( '.$upc.' ) has been updated.');
 
- 
+
 
     }
 
@@ -402,7 +402,7 @@ return $upcs['upcs'];
     public function category(Request $request)
     {
 
-       
+
 $upc = $request->input('upc');
 
 $result = Upc::search($request->get('upc'))->paginate(10);
@@ -418,24 +418,24 @@ $result = Upc::search($request->get('upc'))->paginate(10);
 //         ->orWhere('benefit_uom_wow',$upc)->get();
 
 return view('search')->with(['upcs'=>$result,'upc'=>$upc]);
-  
-        
+
+
     }
 
- 
- 
+
+
     public function noupc()
     {
 
-       
+
        // return view('search')->with('upcs','') ;
         return view('searchsimple')->with(['upc'=>'', 'upcs'=>'']) ;
 
         // return "test";
 
     }
- 
- 
+
+
     public function barcode($cate)
     {
         //$this->cate = $cate;
@@ -445,14 +445,14 @@ return view('search')->with(['upcs'=>$result,'upc'=>$upc]);
         "cate_desc",
         "subcategory",
         "subcate_desc",
-       
+
         "description",
-  
+
         "uom",
-    
+
         "state",
         "exchange",
-       
+
         ) -> where('category', $cate) -> get() ;
 
       //  return view('temp')->with($upcs);
